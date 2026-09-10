4d90fecdac18207e710034c0c04a263e9a8d4343
ChanTrail <125936974+ChanTrail@users.noreply.github.com>
2026-09-10 21:29:14 +0800

    Add concurrency field to main.rs

--- module-template/src/main.rs
+++ module-template/src/main.rs
@@ -72,6 +72,7 @@ const DESCRIBE: &str = r#"{
   "inputTypes": ["video_file"],
   "outputTypes": ["video_file"],
   "official": false,
+  "concurrency": 0,
   "params": [
     {
       "key": "my_param",
